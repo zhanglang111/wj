3f1d137cb0a18e25d9c2612269f07fb1d467aad8
zhanglang111 <38723282+zhanglang111@users.noreply.github.com>
2020-02-25 19:26:41 +0800

    仿写的登陆逻辑

--- src/main/java/com/evan/wj/Reaml/UserRealm.java
+++ src/main/java/com/evan/wj/Reaml/UserRealm.java
@@ -1,24 +1,40 @@
 package com.evan.wj.Reaml;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.evan.wj.Pojo.User;
+import com.evan.wj.Service.UserService;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ObjectUtils;
 
 /**
  * Code by langlang on 2020/2/25
  */
 public class UserRealm extends AuthorizingRealm {
 
+    @Autowired
+    UserService userService;
+
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        //授权逻辑
         return null;
     }
 
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        return null;
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        //认证逻辑
+        String userName = token.getPrincipal().toString();
+        User user = userService.getUserByname(userName);
+        if (ObjectUtils.isEmpty(user)) {
+            throw new UnknownAccountException();
+        }
+        String passwordInDB = user.getPassword();
+        String salt = user.getSalt();
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(userName, passwordInDB, ByteSource.Util.bytes(salt), getName());
+        return authenticationInfo;
     }
 }
